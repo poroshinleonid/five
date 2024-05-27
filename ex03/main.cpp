26aec9c47df0dea9c3062cbf4652783376ef9c89
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-05-27 17:03:14 +0200

    add tests to main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,12 +4,23 @@
 #include "PresidentialPardonForm.h"
 #include "RobotomyRequestForm.h"
 #include "ShrubberyCreationForm.h"
+#include <iostream>
 
 int main() {
-  {
-    Intern someRandomIntern;
-    AForm *rrf;
-    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-    rrf = someRandomIntern.makeForm("robotomy requesst", "Bender");
-  }
+  Intern someRandomIntern;
+  AForm *rrf;
+  rrf = someRandomIntern.makeForm("robotomy requesst", "Bender");
+  rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+  std::cout << rrf->getName() << std::endl;
+  std::cout << rrf->getGradeToExecute() << std::endl;
+  std::cout << rrf->getGradeToSign() << std::endl;
+
+  Bureaucrat president("Obama", 1);
+  rrf->beSigned(president);
+  rrf->execute(president);
+  rrf->execute(president);
+  rrf->execute(president);
+  rrf->execute(president);
+  rrf->execute(president);
+  rrf->execute(president);
 }
