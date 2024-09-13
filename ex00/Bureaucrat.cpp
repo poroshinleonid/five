6cf500ad1dd53516d90bad80690978fce9b576b9
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-09-13 19:28:37 +0200

    ex00 move exception from header to cpp file

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -45,6 +45,14 @@ void Bureaucrat::demote() {
   ++grade_;
 }
 
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+  return "Grade too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+  return "Grade too low";
+}
+
 std::ostream &operator<<(std::ostream &out_stream,
                          const Bureaucrat &bureaucrat) {
   out_stream << bureaucrat.getName();
